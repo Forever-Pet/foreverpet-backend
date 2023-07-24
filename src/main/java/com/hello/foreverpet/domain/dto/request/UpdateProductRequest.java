f4c24f4729482b7120ee72bc271fe62f2bd2332d
cmong0516 <cmong0516@gmail.com>
2023-07-24 11:58:51 +0900

    fix(Product): Product
    Fix Product Categories
    - fix Product Categories Request String to Enum

--- src/main/java/com/hello/foreverpet/domain/dto/request/UpdateProductRequest.java
+++ src/main/java/com/hello/foreverpet/domain/dto/request/UpdateProductRequest.java
@@ -1,6 +1,5 @@
 package com.hello.foreverpet.domain.dto.request;
 
-import com.hello.foreverpet.domain.dto.Categories;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,8 +8,8 @@ import lombok.Data;
 public class UpdateProductRequest {
     private String productName;
     private String productDescription;
-    // String 으로 받아서 Categories name 과 비교후 작업 필요
-    private Categories categories;
+    private String categories;
     private Long productPrice;
     private String productImage;
+    private String brandName;
 }
