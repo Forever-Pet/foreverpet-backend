3faa4910baec98217ccad2e2c93f6f93b70ba1c9
cmong0516 <cmong0516@gmail.com>
2023-08-04 19:09:14 +0900

    feat(Cart & Wish): Cart & Wish
    Add Field cart , wish

--- src/main/java/com/hello/foreverpet/controller/UserController.java
+++ src/main/java/com/hello/foreverpet/controller/UserController.java
@@ -1,17 +1,16 @@
 package com.hello.foreverpet.controller;
 
-import com.hello.foreverpet.domain.dto.request.NewProductRequest;
 import com.hello.foreverpet.domain.dto.request.UserSignupRequest;
 import com.hello.foreverpet.domain.entity.UserInfo;
-import com.hello.foreverpet.service.ProductService;
 import com.hello.foreverpet.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +38,19 @@ public class UserController {
         UserInfo user = new UserInfo();
 
         return ResponseEntity.ok(null);
+    }
+
+    @Operation(summary = "장바구니에 상품 추가", description = "회원의 장바구니에 상품을 추가.")
+    @PostMapping("/cart/{id}")
+    public ResponseEntity<Boolean> addProductInCart(HttpServletRequest httpServletRequest, @PathVariable Long id) {
+        boolean successProductAddCart = userService.addProductInCart(httpServletRequest, id);
+        return ResponseEntity.ok(successProductAddCart);
+    }
+
+    @Operation(summary = "찜목록에 상품 추가", description = "회원의 찜목록에 상품을 추가.")
+    @PostMapping("/wish/{id}")
+    public ResponseEntity<Boolean> addProductInWish(HttpServletRequest httpServletRequest, @PathVariable Long id) {
+        boolean successProductAddWish = userService.addProductInWish(httpServletRequest, id);
+        return ResponseEntity.ok(successProductAddWish);
     }
 }
