a7dd872de9a0293f42bd0f78d5073720cd383de8
jh.lee2 <junheong@nate.com>
2023-08-23 15:30:05 +0900

    fix : 빌드실패로 인한 Test 주석처리

--- src/test/java/com/hello/foreverpet/AuthControllerTest.java
+++ src/test/java/com/hello/foreverpet/AuthControllerTest.java
@@ -16,7 +16,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import springfox.documentation.service.OAuth;
+//import springfox.documentation.service.OAuth;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
